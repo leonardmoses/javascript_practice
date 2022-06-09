018b498f52c3b411db64f80c672709ef37eae0b6
Leonard Moses <qalexeon@polaris.myfiosgateway.com>
2022-06-09 16:38:18 -0400

    Created fibonacci html and js. linked. created headers, buttons, links, and logic. created range adjustment and simple display of sequence.

---
+++ scripts/fibonacci.js
@@ -0,0 +1,46 @@
+let range = 7
+let fibArray = []
+let fibDisplay = document.getElementById('fibSequence')
+
+runSequence(range)
+
+const btnRange = document.getElementById('btnRange')
+btnRange.addEventListener('click', changeRange)
+
+const btnGenerate = document.getElementById('btnGenerate')
+btnGenerate.addEventListener('click', runSequence)
+
+document.getElementById('headerRange').innerHTML = `Your Range is ${range} Numbers`
+
+function changeRange() {
+    range = prompt('How many numbers do you want in your sequence?')
+    document.getElementById('headerRange').innerHTML = `Your Range is ${range} Numbers`
+    console.log(`Range is ${range} numbers.`)
+    runSequence(range)
+}
+
+function runSequence(n) {
+    
+    fibArray = [0,1]
+    let last1
+    let last2
+    let lastNew
+
+    if (n===1) {
+        fibArray = [0]
+    } else if (n===2) {
+        fibArray = [0,1]
+    } else {
+        n = n-2
+        for (let i=0; i<n; i++) {
+            last1 = fibArray[fibArray.length -1]
+            last2 = fibArray[fibArray.length -2]
+            lastNew = last1 + last2
+            fibArray.push(lastNew)
+        }
+    }
+    fibDisplay.innerHTML = fibArray;
+    return fibArray;
+}
+
+fibDisplay.innerHTML = fibArray;
