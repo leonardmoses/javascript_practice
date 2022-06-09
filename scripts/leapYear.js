53644d50a464752fbec669553afdb584cc0ba6b7
Leonard Moses <qalexeon@polaris.myfiosgateway.com>
2022-06-08 22:24:41 -0400

    leap year. added conditionsals.

--- scripts/leapYear.js
+++ scripts/leapYear.js
@@ -5,6 +5,25 @@ askBtn.addEventListener('click', askLeapYear)
 
 function askLeapYear() {
     year = parseInt(prompt('What year would you like to check for a leap year?'))
-    console.log(typeof year)
+    // console.log(typeof year)
+
+
+    if (year%4===0) {
+
+        if (year%100===0) {
+
+            if (year%400===0) {
+                console.log("Yes. condition 3 met")
+            } else {
+                console.log("no. condition 3 not met")
+            }
+            console.log("No. condition 2 met")
+        } else {
+            console.log("Yes. Condition 2 not met")
+        }
+        console.log("yes. Condition 1 met")
+    } else {
+        console.log("No. Condition 1 not met")
+    }
 
 }
