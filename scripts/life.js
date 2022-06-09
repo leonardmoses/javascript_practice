56ba033fb14255a4131fbd252828cd1d34f325f3
Leonard Moses <qalexeon@polaris.myfiosgateway.com>
2022-06-09 13:48:24 -0400

    refactored life remaining. added buttons instead of page load prompt.

--- scripts/life.js
+++ scripts/life.js
@@ -1,19 +1,49 @@
-var maxYear = prompt("In years, what age would you like to live till?")
-var age = prompt("In years, how old are you?")
-document.getElementById("maxYears").innerHTML = maxYear
+let maxYear = null
+let age = null
+let years = null
+let months = null
+let weeks = null
+let days = null
+let hours = null
+let minutes = null
+let seconds = null
 
-function lifeInWeeks(age) {
-  
-    var years = (maxYear-age)
-    var days = (maxYear*365)-(age*365)
-    var weeks = (maxYear*52)-(age*52)
-    var months = (maxYear*12)-(age*12)
+const btnLifeExpectancy = document.getElementById('lifeExpectancy')
+btnLifeExpectancy.addEventListener('click' , maxYearQuestion)
 
-    document.getElementById('sentence').innerHTML = `If you lived till ${maxYear} years old.`
-    document.getElementById('years').innerHTML = `${years} years`
-    document.getElementById('months').innerHTML = `${months} months`
-    document.getElementById('weeks').innerHTML = `${weeks} weeks`
-    document.getElementById('days').innerHTML = `${days} days`
-  }
-  
-lifeInWeeks(age);
+const btnCurrentAge = document.getElementById('currentAge')
+btnCurrentAge.addEventListener('click' , currentAgeQuestion)
+
+function maxYearQuestion() {
+  maxYear = prompt("In years, what age would you like to live till?")
+  document.getElementById('sentence').innerHTML = `If you lived till ${maxYear} years old.`
+  console.log(maxYear)
+}
+
+function calculations() {
+  years = (maxYear-age)
+  months = (maxYear*12)-(age*12)
+  weeks = (maxYear*52)-(age*52)
+  days = (maxYear*365)-(age*365)
+  hours = (maxYear*365*24)-(age*365*24)
+  minutes = (maxYear*365*24*60)-(age*365*24*60)
+  seconds = (maxYear*365*24*60*60)-(age*365*24*60*60)
+}
+
+function currentAgeQuestion() {
+  age = prompt("In years, how old are you?")
+  console.log(age)
+  calculations()
+  display()
+}
+
+function display() {
+  document.getElementById('years').innerHTML = `In Years: ${years} Years`
+  document.getElementById('months').innerHTML = `In Months: ${months} Months`
+  document.getElementById('weeks').innerHTML = `In Weeks: ${weeks} Weeks`
+  document.getElementById('days').innerHTML = `In Days: ${days} Days`
+  document.getElementById('hours').innerHTML = `In Hours: ${hours} Hours`
+  document.getElementById('minutes').innerHTML = `In Minutes: ${minutes} Minutes`
+  document.getElementById('seconds').innerHTML = `In Seconds: ${seconds} Seconds`
+}
+
