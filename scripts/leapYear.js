d2f0e8766d7651af069b526c5316efae28483153
Leonard Moses <qalexeon@polaris.myfiosgateway.com>
2022-06-09 13:09:16 -0400

    conditional logic applied with html statement. logic checked and verified.

--- scripts/leapYear.js
+++ scripts/leapYear.js
@@ -1,29 +1,45 @@
 let year = ''
+let answer = ''
+let answerYes = ''
+let answerNo = ''
 
 askBtn = document.getElementById('askBtn')
 askBtn.addEventListener('click', askLeapYear)
 
 function askLeapYear() {
     year = parseInt(prompt('What year would you like to check for a leap year?'))
-    // console.log(typeof year)
-
+    console.log(year)
+    
+    answerYes = `${year} is a Leap Year!`
+    answerNo = `${year} is not a Leap Year.`
 
     if (year%4===0) {
+        console.log("Yes. Condition 1 met. Year is divisible by 4")
+        
 
-        if (year%100===0) {
+            if (year%100===0) {
+                console.log("No. Condition 2 met. Year that is divisible by 4 is also divisible by 100.")
+                
 
-            if (year%400===0) {
-                console.log("Yes. condition 3 met")
+                    if (year%400===0) {
+                        console.log("Yes. Condition 3 met. Year that is divisble by 4 and 100 is also divisible by 400.")
+                        answer = answerYes
+
+                    } else {
+                        console.log("No. condition 3 not met. Year that is divisible by 4 and 100 is not divisble by 400.")
+                        answer = answerNo
+                    }
+
             } else {
-                console.log("no. condition 3 not met")
+                console.log("Yes. Condition 2 not met. Year taht is divisible by 4 is not divisible by 100.")
+                answer = answerYes
             }
-            console.log("No. condition 2 met")
-        } else {
-            console.log("Yes. Condition 2 not met")
-        }
-        console.log("yes. Condition 1 met")
+
     } else {
-        console.log("No. Condition 1 not met")
+        console.log("No. Condition 1 not met. Year is not divisible by 4")
+        answer = answerNo
     }
 
+    document.getElementById('answer').innerHTML = answer
 }
+
