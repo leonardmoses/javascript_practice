ebbb5b4f774dce988695efd5a0f58a9783f03fe1
Leonard Moses <qalexeon@Polaris.lan>
2022-06-10 15:12:00 -0400

    working on submitting team. Able to console.log created list items.

--- scripts/marvelTeam.js
+++ scripts/marvelTeam.js
@@ -1,6 +1,10 @@
 let teamName = document.querySelector('#teamName')
+
 let btnTeamName = document.querySelector('#btnTeamName')
 btnTeamName.addEventListener('click' , changeTeamName)
+
+let btnSubmitTeam = document.querySelector('#btnSubmitTeam')
+btnSubmitTeam.addEventListener('click', submitTeam)
 
 function changeTeamName() {
     teamName.innerHTML = prompt('Name your team!')
@@ -13,4 +17,15 @@ function addCharacter() {
     let textNode = document.createTextNode(document.getElementById('inputType').value)
     node.appendChild(textNode)
     charListUL.appendChild(node)
+    document.getElementById('inputType').value = ''
+}
+
+function submitTeam() {
+    const charItems = document.querySelectorAll('li')
+    charItems.className = "charClass"
+    console.log(charItems)
+
+    for (let i=0; i<charItems.length; i++) {
+        document.createElement('h3')
+    }
 }
