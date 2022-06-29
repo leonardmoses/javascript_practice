2ee10bc06069302d5a7b77770b484fc026d7f0ce
Leonard Moses <qalexeon@Polaris.lan>
2022-06-29 13:56:57 -0400

    creating computations in the console

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,18 +1,34 @@
-let inputType = document.querySelector('#inputType').innerHTML
+// let inputType = document.querySelector('#inputType').innerHTML
 
+let num1 = 0
+let num2 = 0
+let sum = 0
+const numArray = []
+
+// creating equals buttons
 let inputEnter = document.querySelector('#btnEnter')
 inputEnter.addEventListener('click', btnPress)
 
+//creating display screen area
 let display = document.querySelector('#display')
 
+//creating buttons 0-9 class
 let btnNum1 = document.querySelectorAll('.btnNum')
 
+//click event for buttons 0-9
 for (let i=0; i<btnNum1.length; i++) {
     btnNum1[i].addEventListener('click', (e)=>{
-        display.innerHTML = e.target.innerHTML
+        display.innerHTML = e.target.innerHTML;
+        numArray.push(parseInt(e.target.innerHTML))
+        num1 = numArray[0]
+        num2 = numArray[1]
+        console.log(numArray)
+        console.log(num1)
+        console.log(num2)
     })
 }
 
+//click event for buttons operators
 let btnOperator = document.querySelectorAll('.btnOperator')
 for (let i=0; i<btnOperator.length; i++) {
     btnOperator[i].addEventListener('click', (e)=>{
@@ -20,9 +36,44 @@ for (let i=0; i<btnOperator.length; i++) {
     })
 }
 
+//function to display when button is clicked
 function btnPress() {
     display.innerHTML = document.querySelector('.btnNum').innerHTML
-
 }
 
-console.log(inputType);
+function add(args) {
+    console.log(args)
+    for (let i=0; i<args.length; i++) {
+        sum += args[i]
+    }
+    return sum
+}
+
+function subtract(args) {
+    sum = args[0]
+    console.log(sum)
+    console.log(args)
+    for (let i=1; i<args.length; i++) {
+        sum -= args[i]
+    }
+    return sum
+}
+
+function multiply(args) {
+    for (let i=0; i<args.length; i++) {
+        sum *= args[i]
+    }
+    return sum
+}
+
+function divide(args) {
+    for (let i=0; i<args.length; i++) {
+        sum /= args[i]
+    }
+    return sum
+}
+
+function calculator(operator, ...args) {
+    return operator(args)
+}
+
